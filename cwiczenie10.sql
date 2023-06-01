a01ec61d7eb847f831e1495b9b788745b3eb7f6c
sabatzz <127225789+sabatzz@users.noreply.github.com>
2023-06-01 18:24:39 +0200

    cwiczenie10.sql

--- cwiczenie10.sql
+++ cwiczenie10.sql
@@ -2,9 +2,9 @@ USE AdventureWorks2019;
 
 --ZADANIE 1-------------------------------------------------------------
 
--- Napisz zapytanie, które wykorzystuje transakcjê (zaczyna j¹), a nastêpnie
---aktualizuje cenê produktu o ProductID równym 680 w tabeli Production.Product
---o 10% i nastêpnie zatwierdza transakcjê.
+-- Napisz zapytanie, ktÃ³re wykorzystuje transakcjÄ™ (zaczyna jÄ…), a nastÄ™pnie
+--aktualizuje cenÄ™ produktu o ProductID rÃ³wnym 680 w tabeli Production.Product
+--o 10% i nastÄ™pnie zatwierdza transakcjÄ™.
 
 SELECT * FROM Production.Product WHERE ProductID = 680
 
@@ -19,12 +19,12 @@ SELECT * FROM Production.Product
 
 --ZADANIE 2----------------------------------------------------------------
 
--- Napisz zapytanie, które zaczyna transakcjê, usuwa produkt o ProductID równym
---707 z tabeli Production.Product, ale nastêpnie wycofuje transakcjê. 
+-- Napisz zapytanie, ktÃ³re zaczyna transakcjÄ™, usuwa produkt o ProductID rÃ³wnym
+--707 z tabeli Production.Product, ale nastÄ™pnie wycofuje transakcjÄ™. 
 
 SELECT * FROM Production.Product WHERE ProductID = 707;
 
---Wy³¹czenie sprawdzania ograniczeñ (kluczy obcych, regu³)
+--WyÅ‚Ä…czenie sprawdzania ograniczeÅ„ (kluczy obcych, reguÅ‚)
 EXEC sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all";  
 
 BEGIN TRANSACTION;
@@ -36,10 +36,10 @@ ROLLBACK TRANSACTION;
 
 --ZADANIE 3------------------------------------------------------------------
 
--- Napisz zapytanie, które zaczyna transakcjê, dodaje nowy produkt do tabeli
---Production.Product, a nastêpnie zatwierdza transakcjê 
+-- Napisz zapytanie, ktÃ³re zaczyna transakcjÄ™, dodaje nowy produkt do tabeli
+--Production.Product, a nastÄ™pnie zatwierdza transakcjÄ™ 
 
---W³¹czenie mo¿liwoœci wstawiania kolumn
+--WÅ‚Ä…czenie moÅ¼liwoÅ›ci wstawiania kolumn
 SET IDENTITY_INSERT Production.Product ON 
 
 BEGIN TRANSACTION;
@@ -53,10 +53,10 @@ SELECT * FROM Production.Product WHERE ProductID = 1000;
 
 --ZADANIE 4-------------------------------------------------------------------
 
---Napisz zapytanie, które zaczyna transakcjê i aktualizuje StandardCost wszystkich
---produktów w tabeli Production.Product o 10%, je¿eli suma wszystkich
+--Napisz zapytanie, ktÃ³re zaczyna transakcjÄ™ i aktualizuje StandardCost wszystkich
+--produktÃ³w w tabeli Production.Product o 10%, jeÅ¼eli suma wszystkich
 --StandardCost po aktualizacji nie przekracza 50000. W przeciwnym razie zapytanie
---powinno wycofaæ transakcjê.
+--powinno wycofaÄ‡ transakcjÄ™.
 
 BEGIN TRANSACTION;
 
@@ -68,34 +68,34 @@ SET StandardCost = StandardCost * 1.1;
 DECLARE @TotalCost DECIMAL(10, 2);
 SELECT @TotalCost = SUM(StandardCost) FROM Production.Product;
 
--- Warunek sprawdzaj¹cy sumê i zatwierdzenie lub wycofanie transakcji
+-- Warunek sprawdzajÄ…cy sumÄ™ i zatwierdzenie lub wycofanie transakcji
 IF @TotalCost <= 50000
 BEGIN
     COMMIT TRANSACTION;
-    PRINT 'Transakcja zosta³a zatwierdzona.';
+    PRINT 'Transakcja zostaÅ‚a zatwierdzona.';
 END
 ELSE
 BEGIN
     ROLLBACK TRANSACTION;
-    PRINT 'Transakcja zosta³a wycofana, poniewa¿ suma StandardCost przekroczy³a 50000.';
+    PRINT 'Transakcja zostaÅ‚a wycofana, poniewaÅ¼ suma StandardCost przekroczyÅ‚a 50000.';
 END
 
 --ZADANIE 5------------------------------------------------------------------
 
---Napisz zapytanie SQL, które zaczyna transakcjê i próbuje dodaæ nowy produkt do
---tabeli Production.Product. Jeœli ProductNumber ju¿ istnieje w tabeli, zapytanie
---powinno wycofaæ transakcjê. 
+--Napisz zapytanie SQL, ktÃ³re zaczyna transakcjÄ™ i prÃ³buje dodaÄ‡ nowy produkt do
+--tabeli Production.Product. JeÅ›li ProductNumber juÅ¼ istnieje w tabeli, zapytanie
+--powinno wycofaÄ‡ transakcjÄ™. 
 
---W³¹czenie mo¿liwoœci wstawiania kolumn
+--WÅ‚Ä…czenie moÅ¼liwoÅ›ci wstawiania kolumn
 SET IDENTITY_INSERT Production.Product ON 
 
 BEGIN TRANSACTION;
 
--- Sprawdzenie, czy ProductNumber ju¿ istnieje w tabeli
+-- Sprawdzenie, czy ProductNumber juÅ¼ istnieje w tabeli
 IF EXISTS (SELECT 1 FROM Production.Product WHERE ProductNumber = 'NP-123')
 BEGIN
     ROLLBACK TRANSACTION;
-    PRINT 'Transakcja zosta³a wycofana, poniewa¿ ProductNumber ju¿ istnieje w tabeli.';
+    PRINT 'Transakcja zostaÅ‚a wycofana, poniewaÅ¼ ProductNumber juÅ¼ istnieje w tabeli.';
 END
 ELSE
 BEGIN
@@ -104,61 +104,63 @@ BEGIN
 	VALUES (1001, 'Nowszy produkt', 'NP-123', 1, 1, 350, 250, 3000, 2500, 30, '2013-05-30 00:00:00.000', 'F5752C9C-56BA-41A7-83FD-139DA28C15FC', '2014-02-08 10:01:36.827');
 
     COMMIT TRANSACTION;
-    PRINT 'Transakcja zosta³a zatwierdzona. Dodano nowy produkt.';
+    PRINT 'Transakcja zostaÅ‚a zatwierdzona. Dodano nowy produkt.';
 END
 
 --ZADANIE 6---------------------------------------------------------------------
 
---Napisz zapytanie SQL, które zaczyna transakcjê i aktualizuje wartoœæ OrderQty
---dla ka¿dego zamówienia w tabeli Sales.SalesOrderDetail. Je¿eli którykolwiek z
---zamówieñ ma OrderQty równ¹ 0, zapytanie powinno wycofaæ transakcjê.
+--Napisz zapytanie SQL, ktÃ³re zaczyna transakcjÄ™ i aktualizuje wartoÅ›Ä‡ OrderQty
+--dla kaÅ¼dego zamÃ³wienia w tabeli Sales.SalesOrderDetail. JeÅ¼eli ktÃ³rykolwiek z
+--zamÃ³wieÅ„ ma OrderQty rÃ³wnÄ… 0, zapytanie powinno wycofaÄ‡ transakcjÄ™.
 
 BEGIN TRANSACTION;
 
--- Sprawdzenie, czy którykolwiek z zamówieñ ma OrderQty równ¹ 0
+-- Sprawdzenie, czy ktÃ³rykolwiek z zamÃ³wieÅ„ ma OrderQty rÃ³wnÄ… 0
 IF EXISTS (SELECT 1 FROM Sales.SalesOrderDetail WHERE OrderQty = 0)
 BEGIN
     ROLLBACK TRANSACTION;
-    PRINT 'Transakcja wycofana. Jedno z zamówieñ ma OrderQty równ¹ 0.';
+    PRINT 'Transakcja wycofana. Jedno z zamÃ³wieÅ„ ma OrderQty rÃ³wnÄ… 0.';
 END
 ELSE
 BEGIN
-    -- Aktualizacja wartoœci OrderQty dla wszystkich zamówieñ
+    -- Aktualizacja wartoÅ›ci OrderQty dla wszystkich zamÃ³wieÅ„
     UPDATE Sales.SalesOrderDetail
     SET OrderQty = OrderQty + 1; -- dowolna operacja aktualizacji
 
     COMMIT TRANSACTION;
-    PRINT 'Transakcja zatwierdzona. Wartoœci OrderQty zosta³y zaktualizowane dla wszystkich zamówieñ.';
+    PRINT 'Transakcja zatwierdzona. WartoÅ›ci OrderQty zostaÅ‚y zaktualizowane dla wszystkich zamÃ³wieÅ„.';
 END
 
---Napisz zapytanie SQL, które zaczyna transakcjê i usuwa wszystkie produkty,
---których StandardCost jest wy¿szy ni¿ œredni koszt wszystkich produktów w tabeli
---Production.Product. Je¿eli liczba produktów do usuniêcia przekracza 10,
---zapytanie powinno wycofaæ transakcjê   
+--ZADANIE 7-----------------------------------------------------------------------
+
+--Napisz zapytanie SQL, ktÃ³re zaczyna transakcjÄ™ i usuwa wszystkie produkty,
+--ktÃ³rych StandardCost jest wyÅ¼szy niÅ¼ Å›redni koszt wszystkich produktÃ³w w tabeli
+--Production.Product. JeÅ¼eli liczba produktÃ³w do usuniÄ™cia przekracza 10,
+--zapytanie powinno wycofaÄ‡ transakcjÄ™   
 
 BEGIN TRANSACTION;
 
--- Obliczenie œredniego kosztu wszystkich produktów
+-- Obliczenie Å›redniego kosztu wszystkich produktÃ³w
 DECLARE @AverageCost DECIMAL(10, 2);
 SELECT @AverageCost = AVG(StandardCost) FROM Production.Product;
 
--- Usuniêcie produktów, których StandardCost jest wy¿szy ni¿ œredni koszt
+-- UsuniÄ™cie produktÃ³w, ktÃ³rych StandardCost jest wyÅ¼szy niÅ¼ Å›redni koszt
 DELETE FROM Production.Product
 WHERE StandardCost > @AverageCost;
 
--- Sprawdzenie liczby usuniêtych produktów
+-- Sprawdzenie liczby usuniÄ™tych produktÃ³w
 DECLARE @DeletedCount INT;
 SELECT @DeletedCount = @@ROWCOUNT;
 
--- Sprawdzenie, czy liczba usuniêtych produktów przekracza 10
+-- Sprawdzenie, czy liczba usuniÄ™tych produktÃ³w przekracza 10
 IF @DeletedCount > 10
 BEGIN
     ROLLBACK TRANSACTION;
-    PRINT 'Transakcja zosta³a wycofana, poniewa¿ liczba produktów do usuniêcia jest wiêksza ni¿ 10.';
+    PRINT 'Transakcja zostaÅ‚a wycofana, poniewaÅ¼ liczba produktÃ³w do usuniÄ™cia jest wiÄ™ksza niÅ¼ 10.';
 END
 ELSE
 BEGIN
     COMMIT TRANSACTION;
-    PRINT 'Transakcja zosta³a zatwierdzona. Usuniêto ' + CAST(@DeletedCount AS VARCHAR) + ' produktów.';
+    PRINT 'Transakcja zostaÅ‚a zatwierdzona. UsuniÄ™to ' + CAST(@DeletedCount AS VARCHAR) + ' produktÃ³w.';
 END
 
